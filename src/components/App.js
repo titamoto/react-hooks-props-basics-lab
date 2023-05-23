c4131283864d22a2e350a730de4c2963e946ff69
Aleksandr Savchenko <titamoto@gmail.com>
2023-05-23 09:34:08 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color="firebrick" name={user.name} city={user.city} />
+      <About
+        bio={user.bio}
+        //bioPassed={user.bio !== null || user.bio !== "" ? true : false}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
